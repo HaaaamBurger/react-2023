73667be8ee83a91ba80cc8ab3ccc78a6a2558ef8
HaaaamBurger <gemasclashes@gmail.com>
2023-07-13 13:52:01 +0300

    init

--- src/components/cars/mainComponent/Cars.js
+++ src/components/cars/mainComponent/Cars.js
@@ -4,9 +4,9 @@ import {fetchServices} from "../services/Fetch.services";
 const Cars = () => {
     const [cars, setCars] = useState([]);
 
-    fetch('owu.linkpc.net/carsAPI/v1/cars')
-        .then(response => console.log(response))
-
+    fetch('http://owu.linkpc.net/carsAPI/v1/cars')
+        .then(response => response.json())
+        .then(data => console.log(data))
     return (
         <div>
 

31b505f39f54f7d62c614e4e9c8ebec964747cff
HaaaamBurger <gemasclashes@gmail.com>
2023-07-24 19:22:02 +0300

    init

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 
 import reportWebVitals from './reportWebVitals';
 import {RouterProvider} from "react-router-dom";
+import {router} from "./components/MainRouter/MainRouter";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- src/layouts/MainLayout/MainLayout.js
+++ src/layouts/MainLayout/MainLayout.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
+import {Outlet} from "react-router-dom";
+
+
 const MainLayout = () => {
     return (
         <div>
-            Hello World!
+            <div className={}>
+                Hello World
+            </div>
+            <div>
+                <Outlet/>
+            </div>
         </div>
     );
 };

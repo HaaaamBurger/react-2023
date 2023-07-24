011d2a2dac1e2ed84d3d54e0845718c0dd2e0e86
HaaaamBurger <gemasclashes@gmail.com>
2023-07-24 19:12:53 +0300

    init

---
+++ src/router.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {MainLayout} from "./layouts/MainLayout/MainLayout";
+
+const Router = () => {
+    return (
+        <div>
+            <MainLayout/>
+        </div>
+    );
+};
+
+export {
+    Router,
+};

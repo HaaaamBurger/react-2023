2e631620a5b34651cd47e8ded3161946b5e1f273
HaaaamBurger <gemasclashes@gmail.com>
2023-07-25 19:57:01 +0300

    init

---
+++ src/components/Main/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import styles from './main.module.css'
+const Main = () => {
+    return (
+        <div className={styles.mainWrapper}>
+            Hello
+        </div>
+    );
+};
+
+export {
+    Main,
+};
--- src/components/MainRouter/MainRouter.js
+++ src/components/MainRouter/MainRouter.js
@@ -2,12 +2,18 @@ import {createBrowserRouter} from "react-router-dom";
 
 import {MainLayout} from "../../layouts/MainLayout/MainLayout";
 import {Movies} from "../Movies/Movies";
+import {Main} from "../Main/Main";
 
 const router = createBrowserRouter([
     {
         path:'',
         element: <MainLayout/>,
         children: [
+            {
+                path: '',
+                element: <Main/>
+            }
+            ,
             {
                 path: 'films',
                 element: <Movies/>

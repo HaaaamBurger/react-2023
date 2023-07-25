1a73562e6ff2e5501814a20eb687de062d0a05fa
サーシャ <gemasclashes@gmail.com>
2023-07-25 10:42:15 +0300

    #

--- src/components/Films.js
+++ src/components/Films.js
@@ -1,6 +1,18 @@
 import React from 'react';
 
 const Films = () => {
+    // const options = {
+    //     method: 'GET',
+    //     headers: {
+    //         accept: 'application/json',
+    //         Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2NTdjN2NhNTZmNDg1OGFkMzMzZGExMzFkOGIwNzk2MiIsInN1YiI6IjY0YmU4OGI3MGVkMmFiMDBlMmRiNmNhNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.a5hagmebzWdbalXyNHDJ32ZpaWTo9PrwxhrnaUPpV10'
+    //     }
+    // };
+    //
+    // fetch('https://api.themoviedb.org/3/discover/movie?page=1', options)
+    //     .then(response => response.json())
+    //     .then(response => console.log(response))
+    //     .catch(err => console.error(err));
     return (
         <div>
             Films
---
+++ src/constants/baseURL.js
@@ -0,0 +1,6 @@
+const baseURL = 'https://api.themoviedb.org/3';
+
+const links = {
+    discoverMovies: '/discover/movives',
+}
+export {baseURL,links};

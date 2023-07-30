f2af9e0ab3d4685ceb6986d515c5382f53f81e18
HaaaamBurger <gemasclashes@gmail.com>
2023-07-30 11:49:33 +0300

    init

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,6 +6,7 @@ import {NavLink, useNavigate} from "react-router-dom";
 const Header = () => {
     const navigation = useNavigate();
 
+
     return (
         <div className={styles.mainLayoutNav}>
             <div className={styles.mainLayoutLogo} onClick={() => {
@@ -16,14 +17,20 @@ const Header = () => {
                     <NavLink to={'/page/1'}>Films</NavLink>
                     <NavLink to={'/favourites'}>Favourites</NavLink>
 
+
                     <button onClick={() => {
                         const mainLayout = document.getElementsByClassName('layoutWrapper')[0];
                         mainLayout.classList.toggle(styles.blackTheme);
-                    }}></button>
+
+                        // let buttonSwitch = document.getElementsByClassName('switchButton')[0];
+                        // buttonSwitch.classList.toggle(styles.switchTheme);
+                        // console.log(buttonSwitch)
+                    }} class={'switchButton'}></button>
                 </div>
 
         </div>
     );
+
 };
 
 export {

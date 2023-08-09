ed2a3cce696282afd87315d829ee458bd09bf2c7
HaaaamBurger <gemasclashes@gmail.com>
2023-08-09 18:47:05 +0300

    init

--- src/redux/reducers/carsReducer.js
+++ src/redux/reducers/carsReducer.js
@@ -13,11 +13,11 @@ const carsReducer = (state = initialState, action) => {
                 ...state,
                 cars: state.cars.filter(car => car.id !== action.payload)
             }
-        // case 'UPDATE_CAR_BY_ID':
-        //     return {
-        //         ...state,
-        //         cars: state.cars.find
-        //     }
+        case 'UPDATE_CAR_BY_ID':
+            const carIndex = state.cars.findIndex(car => car.id === action.payload.carId);
+            state.cars[carIndex] = {id: action.payload.carId, ...action.payload.data};
+
+            return state;
         default:
             return state
     }

a17c9c780d9d637eab9600898ab8c61d284df961
HaaaamBurger <gemasclashes@gmail.com>
2023-08-20 17:02:19 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import {CarForm, Cars} from "./components";
+import {useSelector} from "react-redux";
+import {Loading} from "./components/isLoading/Loading";
 
 function App() {
+    const {isLoading} = useSelector(state => state.cars);
+
   return (
     <div>
-
+      <CarForm/>
+      <hr/>
+      {isLoading && <Loading/>}
+      <Cars/>
     </div>
   );
 }
